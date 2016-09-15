4e07b9c316bd48dd5886bf3bd260830c64f5f903
Blain Smith <rebelgeek@blainsmith.com>
2016-09-15 15:43:47 -0400

    test ReceiveShot

---
+++ battleship/game_test.go
@@ -0,0 +1,26 @@
+package battleship_test
+
+import (
+	"github.com/blainsmith/battleship/battleship"
+	"testing"
+)
+
+func TestReceiveShot(t *testing.T) {
+	game := battleship.NewGame()
+
+	if game.ReceiveShot(&battleship.Coord{Letter: "A", Number: "2"}) != 1 {
+		t.Error("expected a hit")
+	}
+	if game.ReceiveShot(&battleship.Coord{Letter: "C", Number: "2"}) != 1 {
+		t.Error("expected a hit")
+	}
+	if game.ReceiveShot(&battleship.Coord{Letter: "B", Number: "2"}) != 2 {
+		t.Error("expected a sink")
+	}
+	if game.ReceiveShot(&battleship.Coord{Letter: "A", Number: "2"}) != 2 {
+		t.Error("expected a sink")
+	}
+	if game.ReceiveShot(&battleship.Coord{Letter: "D", Number: "2"}) != 0 {
+		t.Error("expected a miss")
+	}
+}
