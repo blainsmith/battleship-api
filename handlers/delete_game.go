5b032cceccac03e97a1b827c04a7812b1ab38be7
Blain Smith <rebelgeek@blainsmith.com>
2016-09-15 20:39:20 -0400

    delete a game from memory

--- handlers/delete_game.go
+++ handlers/delete_game.go
@@ -1,10 +1,14 @@
 package handlers
 
 import (
+	"github.com/acmacalister/helm"
+	"github.com/blainsmith/battleship-api/battleship"
 	"net/http"
 	"net/url"
 )
 
 func DeleteGameHandler(w http.ResponseWriter, r *http.Request, params url.Values) {
+	delete(battleship.Games, params["gameId"][0])
 
+	helm.RespondWithJSON(w, struct{}{}, 201)
 }
