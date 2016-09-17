a6e6261335b99e11473d4e17b63744a350de03d6
Blain Smith <rebelgeek@blainsmith.com>
2016-09-16 21:25:10 -0400

    receive a shot and respond with result

--- handlers/receive_shot.go
+++ handlers/receive_shot.go
@@ -1,10 +1,31 @@
 package handlers
 
 import (
+	"fmt"
+	"github.com/acmacalister/helm"
+	"github.com/blainsmith/battleship-api/battleship"
 	"net/http"
 	"net/url"
 )
 
 func ReceiveShotHandler(w http.ResponseWriter, r *http.Request, params url.Values) {
+	// Get the values from the URL
+	gameId := params["gameId"][0]
+	letter := params["letter"][0]
+	number := params["number"][0]
 
+	// Load the game from memory
+	game := battleship.Games[gameId]
+	if game == nil {
+		helm.RespondWithJSON(w, struct{}{}, 404)
+	} else {
+		// Create a Coord of the values
+		coord := &battleship.Coord{
+			Letter: letter,
+			Number: number,
+		}
+
+		// Recieve the shot and respond with the result
+		helm.RespondWithJSON(w, game.ReceiveShot(coord), 200)
+	}
 }
