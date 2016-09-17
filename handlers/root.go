d57eac3e1137b3c70c30f0d1f2b31118e2d28ebd
Blain Smith <rebelgeek@blainsmith.com>
2016-09-16 20:54:39 -0400

    respond with something

--- handlers/root.go
+++ handlers/root.go
@@ -1,10 +1,11 @@
 package handlers
 
 import (
+	"github.com/acmacalister/helm"
 	"net/http"
 	"net/url"
 )
 
 func RootHandler(w http.ResponseWriter, r *http.Request, params url.Values) {
-
+	helm.RespondWithJSON(w, struct{}{}, 201)
 }
