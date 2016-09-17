53e7ea6eb9aa5b750fa8067e58b97e24181ff640
Blain Smith <rebelgeek@blainsmith.com>
2016-09-16 20:54:54 -0400

    reply with a random coord

--- handlers/send_shot.go
+++ handlers/send_shot.go
@@ -1,10 +1,14 @@
 package handlers
 
 import (
+	"github.com/acmacalister/helm"
+	"github.com/blainsmith/battleship-api/battleship"
 	"net/http"
 	"net/url"
 )
 
 func SendShotHandler(w http.ResponseWriter, r *http.Request, params url.Values) {
+	randomCoord := battleship.RandomCoord()
 
+	helm.RespondWithJSON(w, randomCoord, 200)
 }
